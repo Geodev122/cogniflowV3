d568e760126dcc1b4aa527e7d767e9a060b447b4
Geodev122 <geo.elnajjar@gmail.com>
2025-09-29 14:36:04 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages')
+    }
+  },
   plugins: [
     tsconfigPaths(), // resolve TS path aliases from tsconfig
     react()
