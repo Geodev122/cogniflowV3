e1e2c851bc1c82a2113768676a855204c85bc21c
fedgeedev <fedgee911@gmail.com>
2025-09-30 12:46:10 +0300

    h

--- src/pages/SupportTickets.tsx
+++ src/pages/SupportTickets.tsx
@@ -385,7 +385,7 @@ function Header({ onRefresh, onNewTicketCreated }: { onRefresh: () => void; onNe
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <Button variant="outline" onClick={onRefresh} className="gap-2">
+        <Button variant="outline" onClick={onRefresh} className="gap-2" type="button">
           <Loader2 className="h-4 w-4" /> Refresh
         </Button>
         <Dialog open={open} onOpenChange={setOpen}>
